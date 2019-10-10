50425698f1b179b259bd0f33212408f86b8af8f5
Aleksandr Alekseev <hiddenfox29@yandex.ru>
2019-10-10 15:48:43 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// 1кг = 7716ккол
+// 2900 ккал норма
+function cal(){
+    let yourWeight = prompt('ваш вес');
+    let wantWeight = prompt('желаемый вес');
+    let timeToGol = prompt('время достижения в неделях');
+    let result = null;
+    let substraction = 7719-2990;
+    timeToGol = timeToGol * 7;
+    yourWeight = yourWeight * substraction;
+    wantWeight = wantWeight * substraction;
+    if (yourWeight > wantWeight) {
+        result = ((yourWeight - wantWeight)/timeToGol).toFixed(2)
+        alert('вам нужно терять ' + result + ' ккал в день')
+    }
+    else if (yourWeight < wantWeight) {
+        result = ((wantWeight - yourWeight)/timeToGol).toFixed(2)
+        alert('вам нужно набирать ' + result + ' ккал в день')
+
+    } else{alert('вы достигли желаемого')}
+}
+
+cal();
+
+if (confirm('Желаете повторить расчеты?')){
+    cal()
+ }
